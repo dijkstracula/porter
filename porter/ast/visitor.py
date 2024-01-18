367175588b10a58665149ba0a4a199ea0895dad3
Nathan Taylor <ntaylor@cs.utexas.edu>
2024-01-18 14:57:26 -0600

    visitor: make per-node callbacks protected

--- porter/ast/visitor.py
+++ porter/ast/visitor.py
@@ -7,15 +7,18 @@ T = TypeVar("T")
 
 
 class UnimplementedASTNodeHandler(Exception):
-    def __init__(self, cls):
+    def __init__(self, cls: type):
         self.cls = cls
 
     def __str__(self):
-        return f"Unimplemented AST visitor for {str(self.cls)}"
+        return f"Unimplemented AST visitor for {self.cls.__module__}.{self.cls.__name__}"
 
 
 # noinspection PyMethodMayBeStatic,PyShadowingBuiltins
 class Visitor(Generic[T]):
+
+    # Sorts
+
     def visit_sort(self, sort: Sort) -> T:
         match sort:
             case Bool():
@@ -25,10 +28,10 @@ class Visitor(Generic[T]):
             case Enum(discs):
                 return self.enum(discs)
             case Function(domain, range):
-                self.begin_function(sort)
+                self._begin_function(sort)
                 domain = [self.visit_sort(d) for d in domain]
                 range = self.visit_sort(range)
-                return self.finish_function(sort, domain, range)
+                return self._finish_function(sort, domain, range)
             case Numeric(lo, hi):
                 return self.numeric(lo, hi)
             case Uninterpreted():
@@ -44,10 +47,10 @@ class Visitor(Generic[T]):
     def enum(self, discriminants: list[str]):
         raise UnimplementedASTNodeHandler(Enum)
 
-    def begin_function(self, node: Function):
+    def _begin_function(self, node: Function):
         pass
 
-    def finish_function(self, node: Function, domain: list[T], range: T) -> T:
+    def _finish_function(self, node: Function, domain: list[T], range: T) -> T:
         raise UnimplementedASTNodeHandler(Function)
 
     def numeric(self, lo: Optional[int], hi: Optional[int]):
@@ -56,94 +59,93 @@ class Visitor(Generic[T]):
     def uninterpreted(self) -> T:
         raise UnimplementedASTNodeHandler(Uninterpreted)
 
-    #
+    # Expressions
 
     def visit_expr(self, node: Expr) -> T:
         match node:
             case Apply(_, relsym, args):
-                self.begin_apply(node)
+                self._begin_apply(node)
                 relsym = self.visit_expr(relsym)
                 args = [self.visit_expr(arg) for arg in args]
-                return self.finish_apply(node, relsym, args)
+                return self._finish_apply(node, relsym, args)
             case BinOp(_, lhs, _op, rhs):
-                self.begin_binop(node)
+                self._begin_binop(node)
                 lhs_ret = self.visit_expr(lhs)
                 rhs_ret = self.visit_expr(rhs)
-                return self.finish_binop(node, lhs_ret, rhs_ret)
+                return self._finish_binop(node, lhs_ret, rhs_ret)
             case Constant(_, rep):
-                return self.constant(rep)
+                return self._constant(rep)
             case Exists(_, vars, expr):
-                self.begin_exists(node)
+                self._begin_exists(node)
                 vars = [Binding(b.name, self.visit_sort(b.decl)) for b in vars]
                 expr = self.visit_expr(expr)
-                return self.finish_exists(node, vars, expr)
+                return self._finish_exists(node, vars, expr)
             case Forall(_, vars, expr):
-                self.begin_forall(node)
+                self._begin_forall(node)
                 vars = [Binding(b.name, self.visit_sort(b.decl)) for b in vars]
                 expr = self.visit_expr(expr)
-                return self.finish_forall(node, vars, expr)
+                return self._finish_forall(node, vars, expr)
             case Ite(_, test, then, els):
-                self.begin_ite(node)
+                self._begin_ite(node)
                 test = self.visit_expr(test)
                 then = self.visit_expr(then)
                 els = self.visit_expr(els)
-                return self.finish_ite(node, test, then, els)
+                return self._finish_ite(node, test, then, els)
             case Some(_, vars, fmla, _strat):
-                self.begin_some(node)
+                self._begin_some(node)
                 vars = [Binding(b.name, self.visit_sort(b.decl)) for b in vars]
                 fmla = self.visit_expr(fmla)
-                return self.finish_some(node, vars, fmla)
+                return self._finish_some(node, vars, fmla)
             case UnOp(_, _op, expr):
-                self.begin_unop(node)
+                self._begin_unop(node)
                 expr = self.visit_expr(expr)
-                return self.finish_unop(node, expr)
+                return self._finish_unop(node, expr)
         raise Exception(f"TODO: {node}")
 
-    # Terminals
-
-    def constant(self, rep: str) -> T:
+    def _constant(self, rep: str) -> T:
         raise UnimplementedASTNodeHandler(Constant)
 
-    # Non-terminal exprs
-
-    def begin_apply(self, node: Apply):
+    def _begin_apply(self, node: Apply):
         pass
 
-    def finish_apply(self, node: Apply, relsym_ret: T, args_ret: list[T]) -> T:
+    def _finish_apply(self, node: Apply, relsym_ret: T, args_ret: list[T]) -> T:
         raise UnimplementedASTNodeHandler(Apply)
 
-    def begin_binop(self, node: BinOp):
+    def _begin_binop(self, node: BinOp):
         pass
 
-    def finish_binop(self, node: BinOp, lhs_ret: T, rhs_ret: T) -> T:
+    def _finish_binop(self, node: BinOp, lhs_ret: T, rhs_ret: T) -> T:
         raise UnimplementedASTNodeHandler(BinOp)
 
-    def begin_exists(self, node: Exists):
+    def _begin_exists(self, node: Exists):
         pass
 
-    def finish_exists(self, node: Exists, vars: list[Binding[T]], expr: T):
+    def _finish_exists(self, node: Exists, vars: list[Binding[T]], expr: T):
         raise UnimplementedASTNodeHandler(Exists)
 
-    def begin_forall(self, node: Forall):
+    def _begin_forall(self, node: Forall):
         pass
 
-    def finish_forall(self, node: Forall, vars: list[T], expr: T):
+    def _finish_forall(self, node: Forall, vars: list[T], expr: T):
         raise UnimplementedASTNodeHandler(Forall)
 
-    def begin_ite(self, node: Ite):
+    def _begin_ite(self, node: Ite):
         pass
 
-    def finish_ite(self, node: Ite, test: T, then: T, els: T) -> T:
+    def _finish_ite(self, node: Ite, test: T, then: T, els: T) -> T:
         raise UnimplementedASTNodeHandler(Ite)
 
-    def begin_some(self, node: Some):
+    def _begin_some(self, node: Some):
         pass
 
-    def finish_some(self, node: Some, vars: list[Binding[T]], fmla: T):
+    def _finish_some(self, node: Some, vars: list[Binding[T]], fmla: T):
         raise UnimplementedASTNodeHandler(Some)
 
-    def begin_unop(self, node: UnOp):
+    def _begin_unop(self, node: UnOp):
         pass
 
-    def finish_unop(self, node: UnOp, expr: T):
+    def _finish_unop(self, node: UnOp, expr: T):
         raise UnimplementedASTNodeHandler(Some)
+
+    # Actions
+
--- tests/test_visitor.py
+++ tests/test_visitor.py
@@ -5,28 +5,28 @@ from porter.ast.visitor import Visitor
 class ExprCounter(Visitor[None]):
     n_nodes: int = 0
 
-    def constant(self, _rep: str):
+    def _constant(self, _rep: str):
         self.n_nodes += 1
 
-    def finish_apply(self, node: terms.Apply, relsym_ret: None, args_ret: list[None]):
+    def _finish_apply(self, node: terms.Apply, relsym_ret: None, args_ret: list[None]):
         self.n_nodes += 1
 
-    def finish_binop(self, node: terms.BinOp, lhs_ret: None, rhs_ret: None):
+    def _finish_binop(self, node: terms.BinOp, lhs_ret: None, rhs_ret: None):
         self.n_nodes += 1
 
-    def finish_exists(self, node: terms.Exists, vars: list[Binding[None]], expr: None):
+    def _finish_exists(self, node: terms.Exists, vars: list[Binding[None]], expr: None):
         self.n_nodes += 1
 
-    def finish_forall(self, node: terms.Forall, vars: list[Binding[None]], expr: None):
+    def _finish_forall(self, node: terms.Forall, vars: list[Binding[None]], expr: None):
         self.n_nodes += 1
 
-    def finish_ite(self, node: terms.Ite, test: None, then: None, els: None):
+    def _finish_ite(self, node: terms.Ite, test: None, then: None, els: None):
         self.n_nodes += 1
 
-    def finish_some(self, none: terms.Some, vars: list[Binding[None]], fmla: None):
+    def _finish_some(self, none: terms.Some, vars: list[Binding[None]], fmla: None):
         self.n_nodes += 1
 
-    def finish_unop(self, node: terms.UnOp, expr: None):
+    def _finish_unop(self, node: terms.UnOp, expr: None):
         self.n_nodes += 1
 
 def test_expr_visitation():
